c42c07cbd4deebb8eab1123cdc649568eb743932
spa9633 <spa9633@nyu.edu>
2022-05-07 12:15:32 -0400

    Changed int to long

--- Q3_part1.cpp
+++ Q3_part1.cpp
@@ -2,7 +2,7 @@
 #include <math.h>
 
 // function to add the elements of two arrays
-void add(long n, float *x, float *y)
+void add(int n, float *x, float *y)
 {
   for (int i = 0; i < n; i++)
       y[i] = x[i] + y[i];
@@ -10,23 +10,23 @@ void add(long n, float *x, float *y)
 
 int main(int argc, char** argv)
 {
-  long N = atoi(argv[1]) * 1000000; // K*M elements
+  int K = atoi(argv[1]) * 1000000; // K*M elements
 
-  float *x = new float[N];
-  float *y = new float[N];
+  float *x = new float[K];
+  float *y = new float[K];
 
   // initialize x and y arrays on the host
-  for (int i = 0; i < N; i++) {
+  for (int i = 0; i < K; i++) {
     x[i] = 1.0f;
     y[i] = 2.0f;
   }
 
   // Run kernel on 1M elements on the CPU
-  add(N, x, y);
+  add(K, x, y);
 
   // Check for errors (all values should be 3.0f)
   float maxError = 0.0f;
-  for (int i = 0; i < N; i++)
+  for (int i = 0; i < K; i++)
     maxError = fmax(maxError, fabs(y[i]-3.0f));
   std::cout << "Max error: " << maxError << std::endl;
 
