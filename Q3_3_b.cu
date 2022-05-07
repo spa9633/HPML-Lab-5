4a7652fdb0224da8e4b54d804a85570502180568
spa9633 <spa9633@nyu.edu>
2022-05-07 18:01:55 -0400

    Added Q3.3 parts b and c

---
+++ Q3_3_b.cu
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <math.h>
+// Kernel function to add the elements of two arrays
+__global__
+void add(int n, float *x, float *y)
+{
+  int index = threadIdx.x;
+  int stride = blockDim.x;
+  for (int i = index; i < n; i += stride)
+      y[i] = x[i] + y[i];
+}
+
+int main(int argc, char** argv)
+{
+  int K = atoi(argv[1]) * 1000000;
+  float *x, *y;
+
+  // Allocate Unified Memory – accessible from CPU or GPU
+  cudaMallocManaged(&x, K*sizeof(float));
+  cudaMallocManaged(&y, K*sizeof(float));
+
+  // initialize x and y arrays on the host
+  for (int i = 0; i < K; i++) {
+    x[i] = 1.0f;
+    y[i] = 2.0f;
+  }
+
+  // Run kernel on 1M elements on the GPU
+  add<<<1, 256>>>(K, x, y);
+
+  // Wait for GPU to finish before accessing on host
+  cudaDeviceSynchronize();
+
+  // Check for errors (all values should be 3.0f)
+  float maxError = 0.0f;
+  for (int i = 0; i < K; i++)
+    maxError = fmax(maxError, fabs(y[i]-3.0f));
+  std::cout << "Max error: " << maxError << std::endl;
+
+  // Free memory
+  cudaFree(x);
+  cudaFree(y);
+  
+  return 0;
+}
